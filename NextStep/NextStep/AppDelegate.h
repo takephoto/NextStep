683f2b0d75663d7fe9b75981cfee053c31a08155
yang <yang92139@163.com>
2015-12-28 14:36:11 +0800

    1.0.0

---
+++ NextStep/NextStep/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NextStep
+//
+//  Created by 杨洋 on 15/12/28.
+//  Copyright (c) 2015年 杨洋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NextStep/NextStep/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NextStep
+//
+//  Created by 杨洋 on 15/12/28.
+//  Copyright (c) 2015年 杨洋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
